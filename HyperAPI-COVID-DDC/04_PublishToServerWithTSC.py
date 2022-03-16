1eb06f482519051a7b157b0cb407501ab575fc9a
Vorapol <>
2022-03-16 23:09:47 +0700

    Republish For Tableau User Group Thailand

--- HyperAPI-COVID-DDC/04_PublishToServerWithTSC.py
+++ HyperAPI-COVID-DDC/04_PublishToServerWithTSC.py
@@ -1,10 +1,26 @@
 # Sample Code by Vorapol S. (Ping)
+# Need to use Personal Access Token to Authenticate
+# https://help.tableau.com/current/server/en-us/security_personal_access_tokens.htm
+# Need to rename _CONFIG.template.py as _CONFIG.py before using this code
 
+import _CONFIG as config
 import tableauserverclient as TSC
 
-tableau_auth = TSC.TableauAuth('admin', 'bac6343737!', site_id='demo')
-server = TSC.Server('https://www.bac.co.th:8000')
+# Optional Suppress Warning
+
+import urllib3
+urllib3.disable_warnings()
+
+# Authenticate with Tableau Server
+
+tableau_auth = TSC.PersonalAccessTokenAuth(
+    config.tableau_token_name, config.personal_access_token, site_id=config.tableau_site_id
+)
+server = TSC.Server(config.tableau_server)
+server.add_http_options({'verify': False})
 server.auth.sign_in(tableau_auth)
+
+# Get Project ID from Tableau Server
 
 dict_proj = {}
 items, pagination_item = server.projects.get()
@@ -12,13 +28,11 @@ for i in items:
     dict_proj[i.name] = i.id
 
 proj_id = dict_proj.get("Default")
-print(proj_id)
+print(f'"Default" Project ID = {proj_id}')
 
-hyper_file = "/COVID.hyper"
+# Publish Hyper File to Tableau Server
+
+hyper_file = "COVID.hyper"
 new_ds = TSC.DatasourceItem(proj_id)
 new_ds = server.datasources.publish(new_ds, hyper_file, "Overwrite")
-print("Success")
-
-
-
-
+print("Publish Success")
---
+++ HyperAPI-COVID-DDC/_CONFIG.template.py
@@ -0,0 +1,4 @@
+tableau_server = ''
+tableau_site_id = ''
+tableau_token_name = ''
+personal_access_token = ''
